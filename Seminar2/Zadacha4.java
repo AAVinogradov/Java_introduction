80ba6ee1a40ba6bb70904a0355a5e9b67b573899
AAVinogradov <aavinogradov@bk.ru>
2024-02-20 22:50:24 +0300

    добавил решение 4 задачи семинара 2

--- Seminar2/Zadacha4.java
+++ Seminar2/Zadacha4.java
@@ -3,6 +3,45 @@
 // простой текстовый файл, обработайте исключения.
 package Seminar2;
 
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Zadacha4 {
-    
+    public static void main(String[] args) {
+        String resWord = repeatWord("TEST", 100);
+    writeToFile(resWord, "New.txt");
+    }
+    public static String repeatWord(String word, int count){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(word).append(" ");
+        }
+    return sb.toString();
+    }
+    public static void writeToFile(String str, String fileName){
+        try (FileWriter fileWriter = new FileWriter(fileName)){
+            fileWriter.write(str);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+//        FileWriter fileWriter = null;
+//        try {
+//            fileWriter = new FileWriter(fileName);
+//            fileWriter.write(str);
+//        }
+//    catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        finally {
+//          try{
+//              fileWriter.close();
+//            }
+//          catch (Exception e){
+//              e.printStackTrace();
+//          }
+//        }
+
+    }
 }
