43ef543d85c8c417a6b398c90530c2105b1a648b
AAVinogradov <aavinogradov@bk.ru>
2024-02-28 20:20:16 +0300

    выполнил ДЗ 3 семинара 1

--- Seminar1/DZ3.java
+++ Seminar1/DZ3.java
@@ -12,6 +12,36 @@
 // 10.0
 package Seminar1;
 
+import java.util.Scanner;
+
 public class DZ3 {
-    
+    public static void main(String[] args) {
+        calculate();
+    }
+    public static void calculate() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число a: ");
+        double a = scan.nextDouble();
+        System.out.println("Введите операцию + - * /");
+        char c = scan.next().charAt(0);
+        System.out.println("Введите число b: ");
+        double b = scan.nextDouble();
+        scan.close();
+        switch (c) {
+            case '+':
+            System.out.println(a + " + " + b + " = " + (a + b));
+            break;
+            case '-':
+            System.out.println(a + " - " + b + " = " + (a - b));
+            break;
+            case '*':
+            System.out.println(a + " * " + b + " = " + (a * b));
+            break;
+            case '/':
+            System.out.println(a + " / " + b + " = " + (a / b));
+            break;
+            default:
+            System.out.println("Некорректный оператор: " + c);        
+        }
+    }
 }
